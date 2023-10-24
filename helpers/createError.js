85376945d93540485521d2a193fcbf794ec246bd
ING-JORGE2022 <116320081+ING-JORGE2022@users.noreply.github.com>
2023-10-23 23:46:13 -0500

    tarea modulo 3 terminado

--- helpers/createError.js
+++ helpers/createError.js
@@ -1,7 +1,15 @@
-const createError = (message, statusCode) => {
-  const error = new Error(message);
-  error.statusCode = statusCode;
-  return error;
-};
+const messages = {
+    400: 'Bad request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not found',
+    409: 'Conflict',
+}
+
+const createError = (status, message = messages[status]) => {
+    const error = new Error(message);
+    error.status = status;
+    return error;
+}
 
 module.exports = createError;
